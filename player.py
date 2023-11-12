2744abe0d2aa1e3c71f83864a0028bccfb54c90d
kimi5231 <djwjod@naver.com>
2023-11-13 00:59:06 +0900

    캐릭터 왼쪽 이동 구현
    여전히 키를 꾹 눌렀을 때는 계속해서 움직이지 않음

--- player.py
+++ player.py
@@ -1,5 +1,5 @@
 from pico2d import load_image
-from sdl2 import SDL_KEYDOWN, SDLK_RIGHT
+from sdl2 import SDL_KEYDOWN, SDLK_RIGHT, SDLK_LEFT, SDL_KEYUP
 
 
 class Player:
@@ -17,5 +17,12 @@ class Player:
     def handle_event(self, event):
         if event.type == SDL_KEYDOWN:
             if event.key == SDLK_RIGHT:
+                self.dir = 1
                 self.x += self.dir * self.speed
                 self.frame = (self.frame + 1) % 5
+            elif event.key == SDLK_LEFT:
+                self.dir = -1
+                self.x += self.dir * self.speed
+                self.frame = (self.frame + 1) % 5
+        # elif event.type == SDL_KEYUP:
+        #    self.frame = 0
