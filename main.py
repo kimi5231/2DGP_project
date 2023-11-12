e1578bfa6c83a9e46a07b2bb6dd22095b55223f0
kimi5231 <djwjod@naver.com>
2023-11-12 17:48:54 +0900

    handle_events 함수 생성 및 게임 종료 이벤트 활성화

--- main.py
+++ main.py
@@ -1,9 +1,25 @@
 from pico2d import *
 from court import Court
 
+
+def handle_events():
+    global running
+
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            running = False
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
+            running = False
+
+
 open_canvas(1000, 600)
+running = True
 court = Court()
-court.draw()
-update_canvas()
-delay(1.0)
+
+while running:
+    court.draw()
+    update_canvas()
+    handle_events()
+
 close_canvas()
