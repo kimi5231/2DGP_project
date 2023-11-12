62c7605aa5d6c5c519a9e5b88463b2af3233859e
kimi5231 <djwjod@naver.com>
2023-11-13 00:19:53 +0900

    스프라이트 시트 잘라서 그리기 테스트

--- player.py
+++ player.py
@@ -3,7 +3,10 @@ from pico2d import load_image
 
 class Player:
     def __init__(self):
+        self.x = 300
+        self.y = 100
+        self.frame = 0
         self.image = load_image('move.png')
 
     def draw(self):
-        self.image.draw(500, 300)
+        self.image.clip_draw(self.frame * 50, 0, 50, 100, self.x, self.y)
