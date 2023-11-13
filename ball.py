1bd6317fdb854ec3f4ba15526f390d49c3744bc5
kimi5231 <djwjod@naver.com>
2023-11-13 23:10:31 +0900

    Ball 클래스 초기화 설정

--- ball.py
+++ ball.py
@@ -1,3 +1,7 @@
+from pico2d import load_image
+
+
 class Ball:
-    def __init__(self):
-        pass
+    def __init__(self, x, y, dir, speed):
+        self.x, y, dir, speed = x, y, dir, speed
+        self.image = load_image('ball.png')
