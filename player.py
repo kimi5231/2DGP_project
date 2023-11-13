473f246452e648fb74ccfe0b93d379c3c349e6dc
kimi5231 <djwjod@naver.com>
2023-11-13 22:14:44 +0900

    스페이스 키 다운 시 드라이브 서브 대기 자세로 넘어가도록 설정
    계속 움직이지 않고 마지막 장면에서 멈춰있도록 수정 필요

--- player.py
+++ player.py
@@ -1,5 +1,5 @@
 from pico2d import load_image
-from sdl2 import SDL_KEYDOWN, SDLK_RIGHT, SDLK_LEFT, SDL_KEYUP
+from sdl2 import SDL_KEYDOWN, SDLK_RIGHT, SDLK_LEFT, SDL_KEYUP, SDLK_SPACE
 
 PLAYER_H = 110
 MOVE_W = 50
@@ -17,7 +17,7 @@ class Player:
         self.speed = 10
         self.frame = 0
         self.action = 0
-        self.frame_num = 5
+        self.frame_num = MOVE_N
         self.frame_len = MOVE_W
         self.action_len = PLAYER_H
         self.image = load_image('player.png')
@@ -30,9 +30,19 @@ class Player:
     def handle_event(self, event):
         if event.type == SDL_KEYDOWN:
             if event.key == SDLK_RIGHT:
+                self.action = 0
+                self.frame_num = MOVE_N
+                self.frame_len = MOVE_W
                 self.dir = 1
             elif event.key == SDLK_LEFT:
+                self.action = 0
+                self.frame_num = MOVE_N
+                self.frame_len = MOVE_W
                 self.dir = -1
+            elif event.key == SDLK_SPACE:
+                self.action = 1
+                self.frame_num = DRIVE_WAIT_N
+                self.frame_len = DRIVE_W
         elif event.type == SDL_KEYUP:
             self.dir = 0
             self.frame = 0
