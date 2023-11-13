10326701f1c50fa204db462397a750e219be5bf4
kimi5231 <djwjod@naver.com>
2023-11-14 00:22:48 +0900

    스페이스 키 입력 확인 함수 추가

--- player.py
+++ player.py
@@ -26,6 +26,10 @@ def left_up(e):
     return e[0] == 'INPUT' and e[1].type == SDL_KEYUP and e[1].key == SDLK_LEFT
 
 
+def space_down(e):
+    return e[0] == 'INPUT' and e[1].type == SDL_KEYDOWN and e[1].key == SDLK_SPACE
+
+
 class Serve:
     @staticmethod
     def enter(player, e): # Serve 상태로 들어갈 때 할 것
