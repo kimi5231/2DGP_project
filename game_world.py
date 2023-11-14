98ce031ebf3a9429a012fc0a8882903870e4f106
kimi5231 <djwjod@naver.com>
2023-11-14 17:52:31 +0900

    게임 월드에 객체를 추가, 업데이트, 그리기 하는 함수를 추가
    +깊이 표현

---
+++ game_world.py
@@ -0,0 +1,17 @@
+world = [ [], [] ]
+
+
+def add_object(o, depth): # 게임 월드에 객체 담기
+    world[depth].append(o)
+
+    
+def update(): # 게임 월드 객체 업데이트
+    for layer in world:
+        for o in layer:
+            o.update()
+
+
+def render(): # 게임 월드 객체 그리기
+    for layer in world:
+        for o in layer:
+            o.draw()
