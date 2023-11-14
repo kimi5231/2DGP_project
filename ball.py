e257e7a344db220074645eb9ffed2d20c3821400
kimi5231 <djwjod@naver.com>
2023-11-14 18:12:42 +0900

    Ball 클래스에 draw, update 함수 추가

--- ball.py
+++ ball.py
@@ -5,3 +5,9 @@ class Ball:
     def __init__(self, x, y, dir, speed):
         self.x, self.y, self.dir, self.speed = x, y, dir, speed
         self.image = load_image('ball.png')
+
+    def draw(self):
+        self.image.draw(self.x, self.y, 30, 30)
+
+    def update(self):
+        pass
