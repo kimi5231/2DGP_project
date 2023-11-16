c4af2685b554481e199e76243e038332cb4a700e
kimi5231 <djwjod@naver.com>
2023-11-16 11:38:06 +0900

    game_framework 파일 추가

---
+++ game_framework.py
@@ -0,0 +1,18 @@
+def run(mode):
+    pass
+
+
+def quit():
+    pass
+
+
+def change_mode(mode):
+    pass
+
+
+def push_mode(mode):
+    pass
+
+
+def pop_mode():
+    pass
--- play_mode.py
+++ play_mode.py
@@ -33,7 +33,7 @@ def init():
 
 
 def finish():
-    game_world.clear()
+    #game_world.clear()
     pass
 
 
