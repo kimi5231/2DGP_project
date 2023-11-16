f2c2eb2ee700b5dc077165092a3ac320925544ba
kimi5231 <djwjod@naver.com>
2023-11-16 11:30:30 +0900

    main 파일 play_mode, main으로 분할

--- main.py
+++ main.py
@@ -1,52 +1,17 @@
-from pico2d import *
+from pico2d import open_canvas, delay, close_canvas
 
-import game_world
-from court import Court
-from player import Player
-
-
-def handle_events():
-    global running
-
-    events = get_events()
-    for event in events:
-        if event.type == SDL_QUIT:
-            running = False
-        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
-            running = False
-        else:
-            player.handle_event(event)
-
-
-def create_world():
-    global running
-    global court
-    global player
-
-    running = True
-
-    court = Court()
-    game_world.add_object(court, 0)
-
-    player = Player()
-    game_world.add_object(player, 1)
-
-
-def update_world():
-    game_world.update()
-
-
-def render_world():
-    clear_canvas()
-    game_world.render()
-    update_canvas()
-
+import play_mode
 
 open_canvas(1000, 600)
-create_world()
-while running:
-    handle_events()
-    update_world()
-    render_world()
+
+play_mode.init()
+
+while play_mode.running:
+    play_mode.handle_events()
+    play_mode.update_world()
+    play_mode.draw()
     delay(0.05)
+
+play_mode.finish()
+
 close_canvas()
---
+++ play_mode.py
@@ -0,0 +1,55 @@
+from pico2d import *
+
+import game_world
+from court import Court
+from player import Player
+
+
+def handle_events():
+    global running
+
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            running = False
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
+            running = False
+        else:
+            player.handle_event(event)
+
+
+def init():
+    global running
+    global court
+    global player
+
+    running = True
+
+    court = Court()
+    game_world.add_object(court, 0)
+
+    player = Player()
+    game_world.add_object(player, 1)
+
+
+def finish():
+    game_world.clear()
+    pass
+
+
+def update_world():
+    game_world.update()
+
+
+def draw():
+    clear_canvas()
+    game_world.render()
+    update_canvas()
+
+
+def pause():
+    pass
+
+
+def resume():
+    pass
