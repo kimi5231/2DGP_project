df4ff89f5acc710f4ae627fed43d64745f796177
kimi5231 <djwjod@naver.com>
2023-11-16 11:55:11 +0900

    title_mode 파일 추가

--- main.py
+++ main.py
@@ -1,17 +1,20 @@
 from pico2d import open_canvas, delay, close_canvas
 
-import play_mode
+import game_framework
+import title_mode as start_mode
+
 
 open_canvas(1000, 600)
 
-play_mode.init()
-
-while play_mode.running:
-    play_mode.handle_events()
-    play_mode.update_world()
-    play_mode.draw()
-    delay(0.05)
-
-play_mode.finish()
+game_framework.run(start_mode)
+# play_mode.init()
+#
+# while play_mode.running:
+#     play_mode.handle_events()
+#     play_mode.update_world()
+#     play_mode.draw()
+#     delay(0.05)
+#
+# play_mode.finish()
 
 close_canvas()
---
+++ title_mode.py
@@ -0,0 +1,29 @@
+from pico2d import load_image, clear_canvas, update_canvas, get_events
+from sdl2 import SDL_QUIT, SDL_KEYDOWN, SDLK_ESCAPE
+
+import game_framework
+
+
+def init():
+    global image
+    image = load_image('title.png')
+
+
+def finish():
+    global image
+    del image
+
+
+def handle_events():
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            game_framework.quit()
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
+            game_framework.quit()
+
+
+def draw():
+    clear_canvas()
+    image.draw(400, 300)
+    update_canvas()
