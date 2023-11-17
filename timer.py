51c611d112b8d64a02a7d5854a47b22dea256768
kimi5231 <djwjod@naver.com>
2023-11-17 23:52:55 +0900

    타이머의 시간이 1초씩 줄어들게 설정

--- timer.py
+++ timer.py
@@ -3,10 +3,12 @@ from pico2d import load_font, get_time
 
 class Timer:
     def __init__(self):
+        self.sec = 180
         self.font = load_font('ENCR10B.TTF', 16)
 
     def draw(self):
-        self.font.draw(100, 100, f'(Time: {get_time()})', (255, 255, 255))
+        self.font.draw(100, 100, 'Timer', (255, 255, 255))
+        self.font.draw(100, 90, f'{int((180-get_time())//60)} : {int((180-get_time())%60)}', (255, 255, 255))
 
     def update(self):
         pass
