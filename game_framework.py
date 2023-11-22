05236e63566ef041423ef4784fc31c6cb334d785
kimi5231 <djwjod@naver.com>
2023-11-22 23:26:27 +0900

    프레임 시간 설정

--- game_framework.py
+++ game_framework.py
@@ -1,13 +1,23 @@
+import time
+
+
 def run(start_mode):
     global running, stack
     running = True
     stack = [start_mode]
     start_mode.init()
 
+    global frame_time
+    frame_time = 0.0
+    current_time = time.time()
     while running: # 현재 게임 모드에 대한 게임 루프를 실행
         stack[-1].handle_events()
         stack[-1].update()
         stack[-1].draw()
+        frame_time = time.time() - current_time
+        frame_rate = 1.0 / frame_time
+        current_time += frame_time
+        print(f'Frame Time: {frame_time}, Frame Rate: {frame_rate}')
 
     while (len(stack) > 0): # 스택에 남아있는 모든 게임 모드들을 차례대로 제거
         stack[-1].finish()
