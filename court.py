8a1eb254792ca807d26d83905d25dad2a571e10a
kimi5231 <djwjod@naver.com>
2023-11-23 11:59:49 +0900

    court 위치 및 사이즈 조정
    +court 리소스 수정
    +실제 크기에 따른 사이즈 계산

--- court.py
+++ court.py
@@ -3,11 +3,13 @@ from pico2d import load_image
 
 class Court:
     def __init__(self):
-        self.image = load_image('court.png')
+        self.image = load_image('background.png')
+        self.court_image = load_image('court.png')
         self.net_image = load_image('net.png')
 
     def draw(self):
         self.image.draw(500, 300, 1000, 600)
+        self.court_image.draw(500, 51, 600, 10)
         self.net_image.draw(500, 130, 30, 150)
 
     def update(self):
