248d16d5c8b615af62b1ac05d87d1b174442b7e5
kimi5231 <djwjod@naver.com>
2023-11-25 00:42:34 +0900

    공이 거리 내에 있으면 토스 모션을 하도록 설정
    x축 거리를 적당히 조절해야 함
    
    y축 거리도 뭔가 이상함
    
    토스 대기 모션 추가 필요

--- setter.py
+++ setter.py
@@ -1,7 +1,11 @@
 from pico2d import load_image
 
+import game_framework
+import play_mode
 import server
+from behavior_tree import BehaviorTree, Action, Condition, Sequence
 
+PIXEL_PER_METER = (10.0 / 0.3) # 10 pixel 30 cm
 
 # setter action speed
 TIME_PER_ACTION = 0.5
@@ -18,6 +22,7 @@ class Setter:
         self.frame_len = 50
         self.action_len = 110
         self.image = load_image('setter.png')
+        self.build_behavior_tree()
 
     def draw(self):
         sx = self.x - server.background.window_left
@@ -28,7 +33,9 @@ class Setter:
 
 
     def update(self):
-        pass
+        self.frame = ((self.frame + self.frame_num * ACTION_PER_TIME * game_framework.frame_time)
+                        % self.frame_num)
+        self.bt.run()
 
     def get_bb(self):
         sx = self.x - server.background.window_left
@@ -40,4 +47,25 @@ class Setter:
         if group == 'setter:ball':
             pass
 
-    
+    def distance_less_than(self, y1, y2, r):
+        distance2 = y1 - y2
+        return (PIXEL_PER_METER * distance2) < (PIXEL_PER_METER * r)
+
+    def is_ball_over(self, distance):
+        if self.distance_less_than(server.ball.y, self.y, distance):
+            return BehaviorTree.SUCCESS
+        else:
+            return BehaviorTree.FAIL
+
+    def toss(self):
+        self.action = 1
+        self.frame_num = 10
+        self.frame_len = 60
+
+    def build_behavior_tree(self):
+        c1 = Condition('공이 근처에 있는가?', self.is_ball_over, 7)
+        a1 = Action('토스', self.toss)
+
+        root = SEQ_toss_ball = Sequence('공을 토스', c1, a1)
+
+        self.bt = BehaviorTree(root)
