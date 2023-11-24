c5ba8b382b005e9dff6e79166cc69f56732d1d39
kimi5231 <djwjod@naver.com>
2023-11-24 17:51:15 +0900

    ball 중력 속도 (y축 속도) 지정
    원래는 가속도로 해야 하지만 단순하게 하기 위해 속도로 지정

--- ball.py
+++ ball.py
@@ -3,6 +3,13 @@ from sdl2 import SDL_KEYDOWN, SDLK_SPACE
 
 import game_world
 
+# ball gravity speed
+PIXEL_PER_METER = (10.0 / 0.3) # 10 pixel 30 cm
+Gravity_SPEED_KMPH = 36.0 # Km / Hour
+Gravity_SPEED_MPM = (Gravity_SPEED_KMPH * 1000.0 / 60.0)
+Gravity_SPEED_MPS = (Gravity_SPEED_MPM / 60.0)
+Gravity_SPEED_PPS = (Gravity_SPEED_MPS * PIXEL_PER_METER)
+
 
 def space_down(e):
     return e[0] == 'INPUT' and e[1].type == SDL_KEYDOWN and e[1].key == SDLK_SPACE
