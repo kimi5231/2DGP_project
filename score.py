e47a4978170b5b7f88ada2dda9e09d6f60dc5395
kimi5231 <djwjod@naver.com>
2023-11-26 23:59:13 +0900

    점수 갱신 및 세트가 끝날 시 다음 스테이지로 넘어가는 함수 추가

--- score.py
+++ score.py
@@ -1,14 +1,29 @@
 from pico2d import load_font
 
+import server
+
 
 class Score:
     def __init__(self):
         self.player_score = 0
         self.ai_score = 0
+        self.turn = 'player'
         self.font = load_font('ENCR10B.TTF', 30)
 
     def draw(self):
         self.font.draw(260, 550, f'{self.player_score} : {self.ai_score}', (255, 255, 255))
 
     def update(self):
-        pass
+        self.end_set()
+
+    def update_score(self):
+        if self.turn == 'player':
+            self.player_score += 1
+        elif self.turn == 'ai':
+            self.ai_score += 1
+
+    def end_set(self):
+        if self.player_score >= 15 and self.player_score - self.ai_score >= 2:
+            server.stage += 1
+        elif self.ai_score >= 15 and self.ai_score - self.player_score >= 2:
+            pass
