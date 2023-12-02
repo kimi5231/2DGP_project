e590f68ecde5d69d700256fa3a53e0ca1e1046b3
kimi5231 <djwjod@naver.com>
2023-12-02 22:36:45 +0900

    ending_mode bgm 추가

--- ending_mode.py
+++ ending_mode.py
@@ -1,30 +1,34 @@
-from pico2d import load_image, load_font, clear_canvas, update_canvas, get_events, delay, get_time
+from pico2d import load_image, load_font, clear_canvas, update_canvas, get_events, get_time, load_music
 from sdl2 import SDL_QUIT, SDL_KEYDOWN, SDLK_ESCAPE
 
 import game_framework
-import play_mode
-import server
 import title_mode
 
 
 def init():
     global image1, image2
     global font
+    global bgm
     global start_time
 
     image1 = load_image('ending1.png')
     image2 = load_image('ending2.png')
     font = load_font('ENCR10B.TTF')
     start_time = get_time()
+    bgm = load_music('win_music.mp3')
+    bgm.set_volume(32)
+    bgm.repeat_play()
 
 
 def finish():
     global image1
     global image2
     global font
+    global bgm
     del image1
     del image2
     del font
+    del bgm
 
 
 def handle_events():
@@ -39,13 +43,14 @@ def handle_events():
 def update():
     global start_time
 
-    if get_time() - start_time >= 6:
+    if get_time() - start_time >= 11.0:
+        bgm.stop()
         game_framework.change_mode(title_mode)
 
 
 def draw():
     clear_canvas()
-    if get_time() - start_time >= 3:
+    if get_time() - start_time >= 5:
         image2.draw(300, 300, 600, 600)
         font.draw(150, 150, 'YOU GOT THE GOLD MEDAL!!!', (255, 255, 255))
     else:
